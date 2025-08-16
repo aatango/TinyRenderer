b7ca07886bacdd605547f11e4f710942c28a5e88
aatango <80591203+aatango@users.noreply.github.com>
2025-08-16 12:56:01 +0200

    TR "Getting started"
    Keep all the source in a single file. The way Rust handles modules and
    dependencies is confusing me. Since this is a Tiny project, it should be
    OK to keep everything in a single file. This allows me to move on,
    without getting bogged down in making sense of it. If I ever use Rust in
    a larger project, I am sure I will have the opportunity to learn how to
    handle modules and crates and dependencies and what-not.
    
    Export to Netpbm's Portable Pixel Map (PPM) format. The point of this
    project is to make a rendered, not to implement an image format. PPM is
    simple enough that I can define it myself, and not have to use an
    external crate. By doing so, I get to explore a bit of Rust's IO.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,78 @@
 fn main() {
-    println!("Hello, world!");
+    let mut img: Image = Image::blank(64, 64);
+
+    img.set(RED, Position { x: 52, y: 41 });
+
+    export(&img.ppm());
+}
+
+#[derive(Copy, Clone)]
+struct Pixel {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+struct Position {
+    x: usize,
+    y: usize,
+}
+
+struct Image {
+    width: usize,
+    height: usize,
+    data: Vec<Pixel>,
+}
+
+impl Image {
+    fn blank(width: usize, height: usize) -> Self {
+        Self {
+            width,
+            height,
+            data: vec![
+                Pixel {
+                    red: 0,
+                    green: 0,
+                    blue: 0
+                };
+                width * height
+            ],
+        }
+    }
+
+    fn set(&mut self, pixel: Pixel, position: &Position) {
+        if position.x >= self.width || position.y >= self.height {
+            panic!("Setting a pixel outside the image!");
+        }
+
+        self.data[position.x + self.width * position.y] = pixel;
+    }
+
+    fn ppm(&self) -> Vec<u8> {
+        let mut ppm = Vec::new();
+
+        ppm.extend_from_slice(b"P6\n");
+        ppm.extend_from_slice(format!("{} {}\n", self.width, self.height).as_bytes());
+        ppm.extend_from_slice(b"255\n");
+
+        for pixel in &self.data {
+            ppm.push(pixel.red);
+            ppm.push(pixel.green);
+            ppm.push(pixel.blue);
+        }
+
+        ppm
+    }
+}
+
+const RED: Pixel = Pixel {
+    red: 255,
+    green: 0,
+    blue: 0,
+};
+
+fn export(data: &Vec<u8>) {
+    if let Err(e) = std::fs::write("output.ppm", data) {
+        panic!("Error saving to file {}", e);
+    }
 }
