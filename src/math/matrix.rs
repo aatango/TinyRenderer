ea80a81bc0bea71d8817cdbda8aab1ccca87a1ce
aatango <80591203+aatango@users.noreply.github.com>
2025-09-24 20:50:51 +0200

    TR Invert matrices using adjoint method
    Use transpose adjoint matrix to ease calculating the base case of the
    determinant, for a matrix 1x1.
    
    Mostly taken from Sokolov's sample code, in C++.

--- src/math/matrix.rs
+++ src/math/matrix.rs
@@ -1,3 +1,5 @@
+use crate::math::vector::dot_product;
+
 type MatrixNd<const N: usize> = [[f64; N]; N];
 
 pub type Matrix2d = MatrixNd<2>;
@@ -47,6 +49,66 @@ pub fn transpose<const N: usize>(matrix: &MatrixNd<N>) -> MatrixNd<N> {
             result[i][j] = matrix[j][i];
         }
     }
+    result
+}
+
+pub fn invert<const N: usize>(matrix: &MatrixNd<N>) -> MatrixNd<N> {
+    transpose(&invert_transpose(matrix))
+}
+
+#[allow(clippy::needless_range_loop)]
+/// Invert matrix via adjoint method, and use transpose to ease determinant base case.
+fn invert_transpose<const N: usize>(matrix: &MatrixNd<N>) -> MatrixNd<N> {
+    let vector_matrix: Vec<Vec<f64>> = (0..matrix.len())
+        .map(|i| (0..matrix.len()).map(|j| matrix[i][j]).collect())
+        .collect();
+
+    let mut adjugate_transpose = [[0.; N]; N];
+    for i in 0..N {
+        for j in 0..N {
+            adjugate_transpose[i][j] = cofactor(&vector_matrix, i, j);
+        }
+    }
+
+    div(
+        &adjugate_transpose,
+        dot_product(&adjugate_transpose[0], &matrix[0]),
+    )
+}
+
+fn cofactor(matrix: &[Vec<f64>], row: usize, column: usize) -> f64 {
+    let mut submatrix: Vec<Vec<f64>> = vec![vec![0.; matrix.len() - 1]; matrix.len() - 1];
+
+    for i in 0..submatrix.len() {
+        for j in 0..submatrix.len() {
+            submatrix[i][j] = matrix[i + (i >= row) as usize][j + (j >= column) as usize]
+        }
+    }
+
+    det(&submatrix) * (if (row + column) % 2 == 0 { 1. } else { -1. })
+}
+
+fn div<const N: usize>(matrix: &MatrixNd<N>, factor: f64) -> MatrixNd<N> {
+    let mut result = [[0.; N]; N];
+    for i in 0..N {
+        for j in 0..N {
+            result[i][j] = matrix[i][j] / factor;
+        }
+    }
+
+    result
+}
+
+fn det(matrix: &[Vec<f64>]) -> f64 {
+    if matrix.len() == 1 {
+        return matrix[0][0];
+    }
+
+    let mut result: f64 = 0.;
+    for i in 0..matrix.len() {
+        result += matrix[0][i] * cofactor(matrix, 0, i);
+    }
+
     result
 }
 
@@ -117,6 +179,66 @@ mod tests {
                 [4., 8., 12., 16.],
             ];
             assert_eq!(transpose(&matrix), expected);
+        }
+    }
+
+    #[test]
+    fn test_determinant() {
+        {
+            let matrix = vec![vec![2., 1.], vec![1., 3.]];
+            assert_eq!(det(&matrix), 5.);
+        }
+        {
+            let matrix = vec![vec![1., 2., 3.], vec![3., 2., 1.], vec![2., 1., 3.]];
+            assert_eq!(det(&matrix), -12.);
+        }
+    }
+
+    #[test]
+    fn test_cofactor() {
+        let matrix = vec![vec![1., 2., 3.], vec![3., 2., 1.], vec![2., 1., 3.]];
+
+        assert_eq!(cofactor(&matrix, 0, 0), 5.);
+        assert_eq!(cofactor(&matrix, 0, 1), -7.);
+        assert_eq!(cofactor(&matrix, 0, 2), -1.);
+        assert_eq!(cofactor(&matrix, 1, 0), -3.);
+        assert_eq!(cofactor(&matrix, 1, 1), -3.);
+        assert_eq!(cofactor(&matrix, 1, 2), 3.);
+        assert_eq!(cofactor(&matrix, 2, 0), -4.);
+        assert_eq!(cofactor(&matrix, 2, 1), 8.);
+        assert_eq!(cofactor(&matrix, 2, 2), -4.);
+    }
+
+    #[test]
+    fn test_invert() {
+        {
+            let matrix: Matrix2d = [[4., 7.], [2., 6.]];
+            let expected: Matrix2d = [[0.6, -0.7], [-0.2, 0.4]];
+            assert_eq!(invert(&matrix), expected);
+        }
+        {
+            let matrix: Matrix3d = [[1., 2., 3.], [3., 2., 1.], [2., 1., 3.]];
+            let expected: Matrix3d = [
+                [-5. / 12., 1. / 4., 1. / 3.],
+                [7. / 12., 1. / 4., -2. / 3.],
+                [1. / 12., -1. / 4., 1. / 3.],
+            ];
+            assert_eq!(invert(&matrix), expected);
+        }
+        {
+            let matrix: Matrix4d = [
+                [1., 1., 1., -1.],
+                [1., 1., -1., 1.],
+                [1., -1., 1., 1.],
+                [-1., 1., 1., 1.],
+            ];
+            let expected: Matrix4d = [
+                [1. / 4., 1. / 4., 1. / 4., -1. / 4.],
+                [1. / 4., 1. / 4., -1. / 4., 1. / 4.],
+                [1. / 4., -1. / 4., 1. / 4., 1. / 4.],
+                [-1. / 4., 1. / 4., 1. / 4., 1. / 4.],
+            ];
+            assert_eq!(invert(&matrix), expected);
         }
     }
 }
